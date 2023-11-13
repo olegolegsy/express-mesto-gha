71f85e814cb09d608e95008743448111eb852ff4
Sidorov Oleg <ya.sidorovoleg8700@yandex.ru>
2023-11-13 20:52:56 +0300

    14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Авторизуйся!');
+    throw new UnauthorizedError({ message: 'Авторизуйся!' });
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'mesto');
   } catch (err) {
-    throw new UnauthorizedError('Авторизуйся!');
+    throw new UnauthorizedError({ message: 'Авторизуйся!' });
   }
 
   req.user = payload;
